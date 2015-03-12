619a450861072a4449cb435591b39311527fcde5
dorayo <yomingyo@gmail.com>
2015-03-13 00:44:12 +0800

    增加了切换终端为zsh的命令，增加了判断并下载安装oh-my-zsh功能

--- install.sh
+++ install.sh
@@ -6,5 +6,16 @@ ln -sf dotfiles/.bash_profile .
 ln -sf dotfiles/.bashrc_custom .
 ln -sf dotfiles/.vimrc .
 ln -sf dotfiles/.gitconfig .
-ln -sf dotfiles/.zshrc .
 ln -shF dotfiles/.emacs.d ./.emacs.d
+
+## 切换终端为zsh
+chsh -s /bin/zsh
+
+## 下载oh-my-zsh并建立.zshrc的符号链接
+echo "Configuring oh-my-zsh ..."
+if [ -d ~/.oh-my-zsh ];then
+    echo "~/.oh-my-zsh already exist!"
+else
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+ln -sf dotfiles/.zshrc .
